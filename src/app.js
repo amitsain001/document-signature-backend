04d3183537ebcbf5cdadf3737765d62ffad0e9d0
Amit Sain <amit2976297@gmail.com>
2026-03-07 20:50:54 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const signatureRoutes = require("./routes/signatureRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
